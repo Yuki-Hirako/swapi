6c361de7cd1779aa2930351682ddc1241107b430
Yuki-Hirako <yukarihirako.13@gmail.com>
2023-10-28 20:09:17 -0300

    fix: background

--- swapi/src/app/character/[id]/page.js
+++ swapi/src/app/character/[id]/page.js
@@ -26,7 +26,7 @@ export default function CharacterPage() {
     );
 
   return (
-    <div className="min-h-screen bg-gray-800 text-white p-5">
+    <div className="flex flex-col bg-gray-800 text-white p-5">
       <h1 className="text-4xl font-bold mb-5">{character.name}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div>
